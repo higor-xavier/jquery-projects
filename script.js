1aefd0da1a7a7d2ca6543b0107f35dd80d462cd4
higor-xavier <higorsxavier@gmail.com>
2023-05-01 21:48:05 -0300

    Document ready and possible bugs

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	function teste() {
+		console.log($('#exemplo'))
+	}
+	teste()
+})
+
+//ou pode usar a versão mais curta(MENOS COMUM)
+// $(function() {
+// 	console.log($('#exemplo'))
+// }
